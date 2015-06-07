89d26b920489b012192d19bab1b3e3d39ff65d9c
Alexander <traincrazyb@aol.com>
2015-06-07 15:39:09 -0400

    Wouldn't it help if I pushed this also? lol

--- src/main/java/com/cricketcraft/chisel/init/ChiselProperties.java
+++ src/main/java/com/cricketcraft/chisel/init/ChiselProperties.java
@@ -1,9 +1,12 @@
 package com.cricketcraft.chisel.init;
 
 import static com.cricketcraft.chisel.block.variant.BlockVariants.*;
+
 import com.cricketcraft.chisel.util.PropertyVariant;
 
 public class ChiselProperties
 {
 	public static final PropertyVariant CLOUD_VARIANTS = PropertyVariant.create("variant", CLOUD_NORMAL, CLOUD_GRID, CLOUD_LARGE, CLOUD_SMALL, CLOUD_VERTICAL);
+
+	public static final PropertyVariant PUMPKINS_VARIANTS = PropertyVariant.create("variant", PUMPKIN_0, PUMPKIN_1, PUMPKIN_2, PUMPKIN_3, PUMPKIN_4, PUMPKIN_5, PUMPKIN_6, PUMPKIN_7, PUMPKIN_8, PUMPKIN_9, PUMPKIN_10, PUMPKIN_11, PUMPKIN_12, PUMPKIN_13, PUMPKIN_14, PUMPKIN_15, PUMPKIN_16);
 }
