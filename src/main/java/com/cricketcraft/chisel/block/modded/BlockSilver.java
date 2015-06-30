dec33851f89b3bad5f8c617b2ec9cea7c371656a
Cricket <bcord@me.com>
2015-06-29 20:45:18 -0400

    Modded classes are done, but aren't they all modded? So meta

--- src/main/java/com/cricketcraft/chisel/block/modded/BlockSilver.java
+++ src/main/java/com/cricketcraft/chisel/block/modded/BlockSilver.java
@@ -1,4 +1,52 @@
 package com.cricketcraft.chisel.block.modded;
 
-public class BlockSilver {
+import com.cricketcraft.chisel.block.metal.BlockCarvableMetal;
+import com.cricketcraft.chisel.init.ChiselProperties;
+import com.cricketcraft.chisel.util.BlockVariant;
+import com.cricketcraft.chisel.util.IBlockWithSubtypes;
+import net.minecraft.block.state.BlockState;
+import net.minecraft.block.state.IBlockState;
+import net.minecraft.creativetab.CreativeTabs;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemStack;
+import net.minecraftforge.fml.relauncher.Side;
+import net.minecraftforge.fml.relauncher.SideOnly;
+
+import java.util.List;
+
+public class BlockSilver extends BlockCarvableMetal implements IBlockWithSubtypes {
+
+    public BlockSilver(){
+        super();
+        setDefaultState(this.getBlockState().getBaseState().withProperty(ChiselProperties.SILVER_VARIANTS, ChiselProperties.SILVER_VARIANTS.fromMeta(0)));
+    }
+
+    @Override
+    @SuppressWarnings({ "rawtypes", "unchecked" })
+    @SideOnly(Side.CLIENT)
+    public void getSubBlocks(Item itemIn, CreativeTabs tab, List list) {
+        for (BlockVariant variant : ChiselProperties.SILVER_VARIANTS.getAllowedValues()) {
+            list.add(new ItemStack(itemIn, 1, variant.getMeta()));
+        }
+    }
+
+    @Override
+    public IBlockState getStateFromMeta(int meta) {
+        return this.getDefaultState().withProperty(ChiselProperties.SILVER_VARIANTS, ChiselProperties.SILVER_VARIANTS.fromMeta(meta));
+    }
+
+    @Override
+    public int getMetaFromState(IBlockState state) {
+        return ((BlockVariant) state.getValue(ChiselProperties.SILVER_VARIANTS)).getMeta();
+    }
+
+    @Override
+    public String getSubtypeUnlocalizedName(ItemStack stack) {
+        return ChiselProperties.SILVER_VARIANTS.fromMeta(stack.getMetadata()).getName();
+    }
+
+    @Override
+    protected BlockState createBlockState() {
+        return new BlockState(this, ChiselProperties.SILVER_VARIANTS);
+    }
 }
--- src/main/java/com/cricketcraft/chisel/block/modded/BlockSteel.java
+++ src/main/java/com/cricketcraft/chisel/block/modded/BlockSteel.java
@@ -1,4 +1,52 @@
 package com.cricketcraft.chisel.block.modded;
 
-public class BlockSteel {
+import com.cricketcraft.chisel.block.metal.BlockCarvableMetal;
+import com.cricketcraft.chisel.init.ChiselProperties;
+import com.cricketcraft.chisel.util.BlockVariant;
+import com.cricketcraft.chisel.util.IBlockWithSubtypes;
+import net.minecraft.block.state.BlockState;
+import net.minecraft.block.state.IBlockState;
+import net.minecraft.creativetab.CreativeTabs;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemStack;
+import net.minecraftforge.fml.relauncher.Side;
+import net.minecraftforge.fml.relauncher.SideOnly;
+
+import java.util.List;
+
+public class BlockSteel extends BlockCarvableMetal implements IBlockWithSubtypes {
+
+    public BlockSteel(){
+        super();
+        setDefaultState(this.getBlockState().getBaseState().withProperty(ChiselProperties.STEEL_VARIANTS, ChiselProperties.STEEL_VARIANTS.fromMeta(0)));
+    }
+
+    @Override
+    @SuppressWarnings({ "rawtypes", "unchecked" })
+    @SideOnly(Side.CLIENT)
+    public void getSubBlocks(Item itemIn, CreativeTabs tab, List list) {
+        for (BlockVariant variant : ChiselProperties.STEEL_VARIANTS.getAllowedValues()) {
+            list.add(new ItemStack(itemIn, 1, variant.getMeta()));
+        }
+    }
+
+    @Override
+    public IBlockState getStateFromMeta(int meta) {
+        return this.getDefaultState().withProperty(ChiselProperties.STEEL_VARIANTS, ChiselProperties.STEEL_VARIANTS.fromMeta(meta));
+    }
+
+    @Override
+    public int getMetaFromState(IBlockState state) {
+        return ((BlockVariant) state.getValue(ChiselProperties.STEEL_VARIANTS)).getMeta();
+    }
+
+    @Override
+    public String getSubtypeUnlocalizedName(ItemStack stack) {
+        return ChiselProperties.STEEL_VARIANTS.fromMeta(stack.getMetadata()).getName();
+    }
+
+    @Override
+    protected BlockState createBlockState() {
+        return new BlockState(this, ChiselProperties.STEEL_VARIANTS);
+    }
 }
