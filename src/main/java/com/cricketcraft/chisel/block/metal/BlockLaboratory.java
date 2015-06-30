14372533dee329b27d2b593b80c2457b7d04cc8d
Cricket <bcord@me.com>
2015-06-29 21:03:15 -0400

    Metal classes done

--- src/main/java/com/cricketcraft/chisel/block/metal/BlockLaboratory.java
+++ src/main/java/com/cricketcraft/chisel/block/metal/BlockLaboratory.java
@@ -1,4 +1,51 @@
 package com.cricketcraft.chisel.block.metal;
 
-public class BlockLaboratory {
+import com.cricketcraft.chisel.init.ChiselProperties;
+import com.cricketcraft.chisel.util.BlockVariant;
+import com.cricketcraft.chisel.util.IBlockWithSubtypes;
+import net.minecraft.block.state.BlockState;
+import net.minecraft.block.state.IBlockState;
+import net.minecraft.creativetab.CreativeTabs;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemStack;
+import net.minecraftforge.fml.relauncher.Side;
+import net.minecraftforge.fml.relauncher.SideOnly;
+
+import java.util.List;
+
+public class BlockLaboratory extends BlockCarvableMetal implements IBlockWithSubtypes {
+
+    public BlockLaboratory(){
+        super();
+        setDefaultState(this.getBlockState().getBaseState().withProperty(ChiselProperties.LABORATORY_VARIANTS, ChiselProperties.LABORATORY_VARIANTS.fromMeta(0)));
+    }
+
+    @Override
+    @SuppressWarnings({ "rawtypes", "unchecked" })
+    @SideOnly(Side.CLIENT)
+    public void getSubBlocks(Item itemIn, CreativeTabs tab, List list) {
+        for (BlockVariant variant : ChiselProperties.LABORATORY_VARIANTS.getAllowedValues()) {
+            list.add(new ItemStack(itemIn, 1, variant.getMeta()));
+        }
+    }
+
+    @Override
+    public IBlockState getStateFromMeta(int meta) {
+        return this.getDefaultState().withProperty(ChiselProperties.LABORATORY_VARIANTS, ChiselProperties.LABORATORY_VARIANTS.fromMeta(meta));
+    }
+
+    @Override
+    public int getMetaFromState(IBlockState state) {
+        return ((BlockVariant) state.getValue(ChiselProperties.LABORATORY_VARIANTS)).getMeta();
+    }
+
+    @Override
+    public String getSubtypeUnlocalizedName(ItemStack stack) {
+        return ChiselProperties.LABORATORY_VARIANTS.fromMeta(stack.getMetadata()).getName();
+    }
+
+    @Override
+    protected BlockState createBlockState() {
+        return new BlockState(this, ChiselProperties.LABORATORY_VARIANTS);
+    }
 }
