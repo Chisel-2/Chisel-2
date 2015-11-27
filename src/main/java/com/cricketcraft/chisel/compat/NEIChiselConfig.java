bc34166580a5d05b6e0f3f4dc72399ad49cd548e
TheCricket <bcord@me.com>
2015-11-26 22:42:08 -0800

    Updated to the latest gradle, forge version, and completed more JSONs

--- src/main/java/com/cricketcraft/chisel/compat/NEIChiselConfig.java
+++ src/main/java/com/cricketcraft/chisel/compat/NEIChiselConfig.java
@@ -2,33 +2,33 @@ package com.cricketcraft.chisel.compat;
 
 import net.minecraft.item.ItemStack;
 import net.minecraftforge.fml.common.registry.GameRegistry;
-import codechicken.nei.api.API;
-import codechicken.nei.api.IConfigureNEI;
+//import codechicken.nei.api.API;
+//import codechicken.nei.api.IConfigureNEI;
 
 import com.cricketcraft.chisel.Chisel;
 import com.cricketcraft.chisel.client.gui.GuiChisel;
 
-public class NEIChiselConfig implements IConfigureNEI {
+public class NEIChiselConfig /**implements IConfigureNEI*/ {
 
-	@Override
+	//@Override
 	public String getName() {
 		return Chisel.MOD_NAME;
 	}
 
-	@Override
+	//@Override
 	public String getVersion() {
 		return Chisel.VERSION;
 	}
 
-	@Override
+	//@Override
 	public void loadConfig() {
-		API.hideItem(new ItemStack(GameRegistry.findBlock("chisel", "limestone_slab_top")));
-		API.hideItem(new ItemStack(GameRegistry.findBlock("chisel", "marble_slab_top")));
-		API.hideItem(new ItemStack(GameRegistry.findBlock("chisel", "marble_pillar_slab_top")));
+		//API.hideItem(new ItemStack(GameRegistry.findBlock("chisel", "limestone_slab_top")));
+		//API.hideItem(new ItemStack(GameRegistry.findBlock("chisel", "marble_slab_top")));
+		//API.hideItem(new ItemStack(GameRegistry.findBlock("chisel", "marble_pillar_slab_top")));
 
 		//RecipeHandlerChisel handler = new RecipeHandlerChisel();
 		//API.registerRecipeHandler(handler);
 		//API.registerUsageHandler(handler);
-		API.setGuiOffset(GuiChisel.class, -50, 40);
+		//API.setGuiOffset(GuiChisel.class, -50, 40);
 	}
 }
